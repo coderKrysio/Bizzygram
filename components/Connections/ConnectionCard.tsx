ded5c558c4767bec01856023a12adf3d04c3b06f
Anushree <anushreejaiswal.srn@gmail.com>
2023-06-10 12:38:12 +0530

    added card component

---
+++ components/Connections/ConnectionCard.tsx
@@ -0,0 +1,18 @@
+const ConnectionCard = () => {
+    return (
+      <div
+      className="h-[225px] w-[375px] m-auto flex flex-col justify-center items-center max-[470px]:scale-[0.87] max-[400px]:scale-[0.85] max-[470px]:m-0"
+      >
+          <div
+          className="bg-[#ffd803] w-[350px] h-[175px] rounded-lg"
+          >
+          </div>
+  
+          <p
+          className="text-center font-medium pt-2"
+          >Date Added: 00/00/000</p>
+      </div>
+    )
+  }
+  
+  export default ConnectionCard
