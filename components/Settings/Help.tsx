f18d14da325500d59f000f5787ceed5158a6de5a
Anushree <anushreejaiswal.srn@gmail.com>
2023-06-10 13:19:55 +0530

    added components

---
+++ components/Settings/Help.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Help = () => {
+    return (
+        <div
+            className='flex flex-col gap-[25px] h-screen w-full pt-[85px] p-7 ml-3 max-[1070px]:pb-[70px] max-[470px]:ml-0'
+            >
+                <h1
+                className='text-3xl font-semibold border-b-2 border-slate-300 pb-5'
+                >Help</h1>
+            </div>
+    )
+}
+
+export default Help
