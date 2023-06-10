aa18cc804588816a616075f09b1db36bd96aea50
Anushree <anushreejaiswal.srn@gmail.com>
2023-06-10 08:31:41 +0530

    added landing page

---
+++ components/LandingPage.tsx
@@ -0,0 +1,18 @@
+const LandingPage = () => {
+    return (
+        <div>
+
+            <h1 className="text-6xl font-semibold text-center">
+                Bizzygram
+            </h1>
+
+            <h3 className="pt-8 text-2xl text-center">
+                Connect with a Tap,
+                <br />
+                Your Network in One Snap!
+            </h3>
+        </div>
+    )
+}
+
+export default LandingPage
