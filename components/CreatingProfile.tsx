0dfca42eb4c298e1be8b7b47d335fa0832d6a753
Anushree <anushreejaiswal.srn@gmail.com>
2023-06-11 00:45:13 +0530

    added components

---
+++ components/CreatingProfile.tsx
@@ -0,0 +1,13 @@
+import ProfileForm from "./ProfileForm/ProfileForm"
+
+const CreatingProfile = () => {
+  return (
+    <div
+    className="bg-[#f3fbfb] text-[#272343] flex w-screen h-screen overflow-hidden justify-center items-center"
+    >
+        <ProfileForm />
+    </div>
+  )
+}
+
+export default CreatingProfile
