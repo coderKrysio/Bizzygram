6428481950c56e9fb05b9f37af81fdbcf5a4fe95
Anushree <anushreejaiswal.srn@gmail.com>
2023-06-14 22:16:07 +0530

    added contact details

--- components/Settings/Help.tsx
+++ components/Settings/Help.tsx
@@ -8,6 +8,13 @@ const Help = () => {
                 <h1
                 className='text-3xl font-semibold border-b-2 border-slate-300 pb-5'
                 >Help</h1>
+
+                <p
+                className='text-xl font-medium text-center'
+                >For any queries, mail us at
+                    <br />
+                    bizzygram@gmail.com
+                </p>
             </div>
     )
 }
